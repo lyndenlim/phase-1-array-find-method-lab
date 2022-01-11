1b65ca9bc6331c84da18285420e8e7f1c44b9dd9
Lynden Lim <lyndenlim15@gmail.com>
2022-01-11 14:49:14 -0500

    commit tree 50878139991116b27cd884a79183425c895f79ea

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(objects){
+    let winYear = objects.find(objects => objects.result === "W")
+    if (winYear){
+        return winYear.year
+    } else {
+        return winYear;
+    };
+};
